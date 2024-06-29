d038eddb8e617dc96836535251c7e2f454ec869c
briankipchirchir <kbrianngetich@gmail.com>
2024-06-29 22:13:10 +0300

    phase-1-scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  console.log(customerName.toUpperCase());
+}
+//
+function setBestCustomer() {
+
+    globalThis.bestCustomer = 'not bob'; 
+    console.log(bestCustomer); 
+  }
+  //
+  function overwriteBestCustomer() {
+
+    globalThis.bestCustomer = 'maybe bob';
+    console.log(bestCustomer); 
+  }
+  //
+  const leastFavoriteCustomer = 'kip';
+
+function changeLeastFavoriteCustomer() {
+  
+  leastFavoriteCustomer = 'orah';
+  console.log(leastFavoriteCustomer); 
+}
+upperCaseCustomerName();
+setBestCustomer();
+overwriteBestCustomer();
+changeLeastFavoriteCustomer();
+
+
